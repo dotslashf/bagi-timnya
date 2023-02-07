e0f0ff33b2acfdbcb8644711fdf6a06429e99b40
fadhlu <38921923+dotslashf@users.noreply.github.com>
2023-02-07 12:03:53 +0800

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import "react-toastify/dist/ReactToastify.css";
 import type { AppProps } from "next/app";
 import { AppWrapper } from "../context/state";
 import { ToastContainer } from "react-toastify";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppWrapper>
       <Component {...pageProps} />
       <ToastContainer />
+      <Analytics />
     </AppWrapper>
   );
 }
